76cc6ff958e7300f429f1727e03815a6b76c46d2
Luc.Tran <anhvanluc1999@gmail.com>
2024-10-28 20:48:49 +0700

    expense - sort by datetime

--- src/hooks/useGetCollection/useGetCollection.tsx
+++ src/hooks/useGetCollection/useGetCollection.tsx
@@ -7,6 +7,15 @@ type useGetCollectionProps = {
     dependencies?: any[];
 }
 
+function sortByDateDescending(data) {
+  return data.sort((a, b) => {
+    // Assuming a.date and b.date are in ISO format or Date objects
+    const dateA: any = new Date(a.date || ""); // Convert date string to Date object
+    const dateB: any = new Date(b.date || ""); // Convert date string to Date object
+    return dateB - dateA; // Sort from latest to oldest
+  });
+}
+
 export const useGetCollection = ({ collectionName, dependencies = [] }: useGetCollectionProps) => {
     const [data, setData] = useState<DocumentData>([]);
     const [loading, setLoading] = useState(true);
@@ -19,7 +28,8 @@ export const useGetCollection = ({ collectionName, dependencies = [] }: useGetCo
               querySnapshot.forEach((doc) => {
                 dataArray.push({ id: doc.id, ...doc.data() });
               });
-              setData(dataArray);
+              const sortedData = sortByDateDescending(dataArray)
+              setData(sortedData);
             } catch (error) {
               console.error("Error fetching documents: ", error);
             } finally {
